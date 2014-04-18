babff23569e1cbcf90b1aadf1b36a15755f1b2f1
Jason Frame <jason@onehackoranother.com>
2014-04-18 09:24:10 +0100

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# map-range
+
+Convert a function that has a domain and range of `0..1` one with an arbitrary domain and range by applying a linear mapping.
---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = function(fn, inStart, inEnd, outStart, outEnd) {
+
+	if (outStart === void 0) {
+		outStart = inStart;
+		outEnd = inEnd;
+		inStart = 0;
+		inEnd = 1;
+	}
+
+	var inRange = inEnd - inStart,
+		outRange = outEnd - outStart;
+
+	return function(val) {
+		var original = fn((val - inStart) / inRange);
+		return outStart + (outRange * original);
+	}
+
+}
